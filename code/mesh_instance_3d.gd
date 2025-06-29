9d9e4283ba40b50c82f6b2b0c1c69cb1a0d8d3a3
Your Name <you@example.com>
2025-06-29 21:32:39 +0200

    Zylinder

--- code/mesh_instance_3d.gd
+++ code/mesh_instance_3d.gd
@@ -1,41 +1,58 @@
 extends MeshInstance3D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	var radius = 2
-	var segments = 10
+	var height = 4  
+	var segments = 32 
 	var delta = 2 * PI / segments
 	
 	var verts = PackedVector3Array()
-	verts.append(Vector3(0, 0, 0))  # Zentrum des Kreises
 	
-	# Generiere die Vertices entlang des Kreises
+
 	for i in range(segments):
 		var x = radius * cos(i * delta)
 		var z = radius * sin(i * delta)
 		verts.append(Vector3(x, 0, z))
 	
+	
+	for i in range(segments):
+		var x = radius * cos(i * delta)
+		var z = radius * sin(i * delta)
+		verts.append(Vector3(x, height, z))
+	
+
 	var indices = PackedInt32Array()
-	# Erstelle die Indizes für die Dreiecke
-	for i in range(1, segments):
-		# Erster Dreieck (0, i, i+1)
+	for i in range(segments):
+		var next_i = (i + 1) % segments 
+	
+		indices.append(i) 
+		indices.append(i + segments) 
+		indices.append(next_i) 
+
+		indices.append(i + segments)  
+		indices.append(next_i + segments) 
+		indices.append(next_i)  
+	
+	
+	var base_start = verts.size() - segments * 2
+	for i in range(segments - 2):
+		# Untere Fläche
 		indices.append(0)
-		indices.append(i)
 		indices.append(i + 1)
-	
-	# Letztes Dreieck (0, segments, 1)
-	indices.append(0)
-	indices.append(segments)
-	indices.append(1)
-	
+		indices.append(i + 2)
+		
+		# Obere Fläche
+		indices.append(segments)
+		indices.append(segments + i + 2)
+		indices.append(segments + i + 1)
+
+
 	var surface_array = []
 	surface_array.resize(Mesh.ARRAY_MAX)
 	surface_array[Mesh.ARRAY_VERTEX] = verts
 	surface_array[Mesh.ARRAY_INDEX] = indices
 	
 	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
-	
-	pass 
 
 func _process(delta: float) -> void:
 	pass
